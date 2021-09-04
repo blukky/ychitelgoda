b41a5548a0d52e7954b1763624ff1adb515ebabb
Dima <ya.gud@mail.ru>
2021-09-04 20:42:12 +0300

    страницы

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, '')
+
+
+def news(request):
+    return render(request, '')
+
+
+def publish(request):
+    return render(request, '')
+
+
+def vmeste(request):
+    return render(request, '')
+
+
+def npa(request):
+    return render(request, '')
+
+
+def visited(request):
+    return render(request, '')
+
+
+def index(request):
+    return render(request, '')
+
+
+def index(request):
+    return render(request, '')
+
+
+def index(request):
+    return render(request, '')
+
 
 def index(request):
     return render(request, '')
