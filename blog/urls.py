ed4a1498967f65f1d130c3ab2b58fa682ee7b797
Dima <ya.gud@mail.ru>
2021-09-26 19:57:36 +0300

    pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,19 @@ from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
-    path('news', news, name='news'),
-    path('publish', publish, name='publish'),
-    path('vmeste', vmeste, name='vmeste'),
-    path('npa', npa, name='npa'),
-    path('visited', visited, name='visited'),
-    path('', index, name='index'),
-    path('', index, name='index'),
-    path('', index, name='index'),
+    path('education', education, name='education'),
+    path('kurses', kurses, name='kurses'),
+    path('advance', advance, name='advance'),
+    path('advance_small', advance_small, name='advance_small'),
+    path('tests', tests, name='tests'),
+    path('kim', kim, name='kim'),
+    path('konkurs', konkurs, name='konkurs'),
+    path('olimpic', olimpic, name='olimpic'),
+    path('work_programm', work_programm, name='work_programm'),
+    path('konspect', konspect, name='konspect'),
+    path('work_with_parent', work_with_parent, name='work_with_parent'),
+    path('present', present, name='present'),
+    path('akcii', akcii, name='akcii'),
+    path('project', project, name='project'),
+    path('gallerey', gallerey, name='gallerey')
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,23 +7,55 @@ def index(request):
     return render(request, 'main.html')
 
 
-def news(request):
-    return render(request, '')
+def education(request):
+    return render(request, 'education.html')
 
 
-def publish(request):
-    return render(request, '')
+def kurses(request):
+    return render(request, 'kurses.html')
 
 
-def vmeste(request):
-    return render(request, '')
+def advance(request):
+    return render(request, 'advance.html')
 
 
-def npa(request):
-    return render(request, '')
+def advance_small(request):
+    return render(request, 'advance_small.html')
 
 
-def visited(request):
-    return render(request, '')
+def tests(request):
+    return render(request, 'tests.html')
+
+
+def kim(request):
+    return render(request, 'kim.html')
+
+def konkurs(request):
+    return render(request, 'konkurs.html')
+
+def olimpic(request):
+    return render(request, 'olipiс.html')
+
+def work_programm(request):
+    return render(request, 'work_programm.html')
+
+def konspect(request):
+    return render(request, 'konspect.html')
+
+def work_with_parent(request):
+    return render(request, 'work_with_parent.html')
+
+def present(request):
+    return render(request, 'precent.html')
+
+def akcii(request):
+    return render(request, 'akcii.html')
+
+def project(request):
+    return render(request, 'project.html')
+
+def gallerey(request):
+    return render(request, 'galler.html')
+
 
 
